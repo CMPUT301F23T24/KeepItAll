68b883047795b9c449fee72b22f498627a6ea2c5
NicroFang <nicrofang@gmail.com>
2023-10-22 13:48:40 -0600

    updating user classes

--- app/src/main/java/com/example/keepitall/User.java
+++ app/src/main/java/com/example/keepitall/User.java
@@ -1,13 +1,16 @@
 package com.example.keepitall;
 
+import java.util.ArrayList;
+
 /**
  * Used for storing all of a users information, including their name and item manager
  */
-public class User {
+public class User  implements Comparable<User>{
 
     // Primary Variables used and help by the User
     private String userName;
     private ItemManager itemManager;
+    private Item selectedItem;      
 
     /**
      * Constructor for the User Object
@@ -17,4 +20,33 @@ public class User {
         this.userName = userName;
         itemManager = new ItemManager();
     }
+
+
+    public Item viewItem(String itemName){
+        // Get the desired item from the itemManager, based on the given name
+        selectedItem = ItemManager.getItem(itemName);
+        // we can now do funcionality with the item we have selected
+    }
+
+    public ArrayList<Item> viewAllItems(){
+        // Returns the list of all of the items within the ItemManager
+    }
+
+    /**
+     * Compares the Username of two different Users, returns 0 if they are equal
+     * This is primarily used if we ever need to sort users
+     * @param userToCompare
+     * @return
+     * 0 -> if they are equal
+     * -1 -> if they are not
+     */
+    @Override public int compareTo(User userToCompare) {
+        return this.userName.compareTo(userToCompare.getUserName());
+    }
+
+
+    // Getters and Setters
+    public String getUserName() {return userName;}
+    public void setUserName(String userName) {this.userName = userName;}
+    public Item getSelectedItem() {return selectedItem;}
 }
