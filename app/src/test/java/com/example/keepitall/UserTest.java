914c5cac6cca7b691513227e6cee68d5ef002a70
Cohen <ccalver1@ualberta.ca>
2023-10-25 12:57:53 -0600

    updates to the user class and userTest class

--- app/src/test/java/com/example/keepitall/UserTest.java
+++ app/src/test/java/com/example/keepitall/UserTest.java
@@ -1,7 +1,24 @@
 package com.example.keepitall;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 /**
  * Test Cases for the User Class
  */
 public class UserTest {
+
+    /**
+     *
+     */
+    @Test
+    public void testCompareTo() {
+        User user1 = new User("Cohen");
+        User user2 = new User("John Smith");
+        User user3 = new User("Cohen");
+
+        assertEquals(0, user1.compareTo(user3)); // user1 == user3 (they have the same name)
+        assertEquals(-1, user1.compareTo(user2)); // user1 != user2 (they have different names)
+    }
 }
