b15b4677c5e602096337b91f19679698cb0f494f
Karl Vinuya <karl_vinuya@yahoo.ca>
2023-10-30 23:02:00 -0600

    Minor changes to viewpage and homepage

--- app/src/main/java/com/example/keepitall/HomePageActivity.java
+++ app/src/main/java/com/example/keepitall/HomePageActivity.java
@@ -3,8 +3,18 @@ package com.example.keepitall;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class HomePageActivity extends AppCompatActivity {
+
+    GridView gridView;
+    String[] items = {"item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8"};
+    int[] images = {R.drawable.app_icon, R.drawable.app_icon, R.drawable.app_icon, R.drawable.app_icon, R.drawable.app_icon, R.drawable.app_icon, R.drawable.app_icon, R.drawable.app_icon};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +28,38 @@ public class HomePageActivity extends AppCompatActivity {
         ///TODO: The key for the username is "username"
         //       feel free to change how its stored, but this line gives you the current
         //       username of the user that is logged in
+
+
+        gridView = findViewById(R.id.gridView);
+        CustomAdapter customAdapter = new CustomAdapter();
+        gridView.setAdapter(customAdapter);
+    }
+
+    private class CustomAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view1 = getLayoutInflater().inflate(R.layout.item_grid, null);
+
+            TextView name = view1.findViewById(R.id.gridDataName);
+            ImageView image = view1.findViewById(R.id.gridDataImage);
+            name.setText(items[position]);
+            image.setImageResource(images[position]);
+            return view1;
+        }
     }
 }
