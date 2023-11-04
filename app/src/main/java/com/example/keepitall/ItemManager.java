cc7b46de322d0c15050b5f2cc496f9388d5c37f8
Karl Vinuya <karl_vinuya@yahoo.ca>
2023-11-04 00:53:54 -0600

    Add HomePage tests

--- app/src/main/java/com/example/keepitall/ItemManager.java
+++ app/src/main/java/com/example/keepitall/ItemManager.java
@@ -1,5 +1,6 @@
 package com.example.keepitall;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -10,7 +11,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
 
-public class ItemManager {
+public class ItemManager implements Serializable {
     private ArrayList<Item> itemList;
 
 
