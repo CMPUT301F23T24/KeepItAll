2a81358831d86a9e548399c2d421bca5f049e11d
Silin Song <silin5@ualberta.ca>
2023-11-20 10:44:44 -0700

    fix:Add the new ItemTagsActivity to AndroidManifest.xml to register it

--- app/src/main/java/com/example/keepitall/ItemTagsActivity.java
+++ app/src/main/java/com/example/keepitall/ItemTagsActivity.java
@@ -7,7 +7,7 @@ public class ItemTagsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.item_tags); 
+        setContentView(R.layout.item_tags);
     }
 }
 
