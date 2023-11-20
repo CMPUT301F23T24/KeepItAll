f789dc9f20ed16ad9a838ed5fc71b81384897109
Silin Song <silin5@ualberta.ca>
2023-11-20 11:00:05 -0700

    fix:make backButton works on item tag page

--- app/src/main/java/com/example/keepitall/ItemTagsActivity.java
+++ app/src/main/java/com/example/keepitall/ItemTagsActivity.java
@@ -1,6 +1,9 @@
 package com.example.keepitall;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ItemTagsActivity extends AppCompatActivity {
@@ -8,6 +11,14 @@ public class ItemTagsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_tags);
+
+        Button backButton = findViewById(R.id.viewBackButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
 
