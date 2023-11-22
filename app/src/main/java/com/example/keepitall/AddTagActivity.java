527689004248a3594b8c1025bf5fd0d34a7ded94
Silin Song <silin5@ualberta.ca>
2023-11-21 22:09:42 -0700

    feat:tag works

--- app/src/main/java/com/example/keepitall/AddTagActivity.java
+++ app/src/main/java/com/example/keepitall/AddTagActivity.java
@@ -1,33 +1,31 @@
 package com.example.keepitall;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddTagActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_tag);
 
+        EditText tagNameEditText = findViewById(R.id.tagText);
+
         Button saveButton = findViewById(R.id.saveButton);
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Logic to save the tag goes here
-                finish();
-            }
+        saveButton.setOnClickListener(v -> {
+            String tagName = tagNameEditText.getText().toString();
+            Intent returnIntent = new Intent();
+            returnIntent.putExtra("newTag", tagName);
+            setResult(RESULT_OK, returnIntent);
+            finish();
         });
 
         Button backButton = findViewById(R.id.viewBackButton);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish(); 
-            }
-        });
+        backButton.setOnClickListener(v -> finish());
     }
 }
-
--- app/src/main/java/com/example/keepitall/ItemTagsActivity.java
+++ app/src/main/java/com/example/keepitall/ItemTagsActivity.java
@@ -1,24 +1,102 @@
 package com.example.keepitall;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class ItemTagsActivity extends AppCompatActivity {
+
+    private GridView gridView;
+    private ArrayList<Tag> tags;
+    private TagAdapter tagAdapter;
+    private ArrayList<Tag> tagsToDelete = new ArrayList<>();
+    private boolean deleteMode = false;
+    private static final int ADD_TAG_REQUEST_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_tags);
 
-        Button backButton = findViewById(R.id.viewBackButton);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
+        // Initialize your tags list
+        tags = new ArrayList<>();
+        // TODO: Populate your tags list here
+
+        // Initialize GridView and its adapter
+        gridView = findViewById(R.id.gridView);
+        tagAdapter = new TagAdapter(this, tags);
+        gridView.setAdapter(tagAdapter);
+
+        // GridView item click listener
+        gridView.setOnItemClickListener((parent, view, position, id) -> {
+            gridViewItemClickEvent(view, position);
         });
+
+        // Add Tag button
+        Button addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(v -> {
+            Intent intent = new Intent(ItemTagsActivity.this, AddTagActivity.class);
+            startActivityForResult(intent, ADD_TAG_REQUEST_CODE);
+        });
+
+        // Delete Tag button
+        Button deleteButton = findViewById(R.id.deleteButton);
+        deleteButton.setOnClickListener(v -> {
+            deleteButtonClickEvent();
+        });
+
+        // Back button
+        Button backButton = findViewById(R.id.viewBackButton);
+        backButton.setOnClickListener(v -> finish());
+    }
+
+    private void gridViewItemClickEvent(View view, int position) {
+        if (deleteMode) {
+            Tag selectedTag = tags.get(position);
+            if (tagsToDelete.contains(selectedTag)) {
+                tagsToDelete.remove(selectedTag);
+                view.setBackgroundColor(Color.TRANSPARENT);
+            } else {
+                tagsToDelete.add(selectedTag);
+                view.setBackgroundColor(Color.LTGRAY);
+            }
+        } else {
+            // Handle non-delete mode item click if necessary
+        }
+    }
+
+    private void deleteButtonClickEvent() {
+        Button deleteButton = findViewById(R.id.deleteButton);
+        if (!deleteMode) {
+            deleteMode = true;
+            Toast.makeText(ItemTagsActivity.this, "Select tags to delete", Toast.LENGTH_SHORT).show();
+            deleteButton.setBackgroundColor(Color.GRAY); // Change button color to indicate delete mode
+        } else {
+            // Delete selected tags
+            tags.removeAll(tagsToDelete);
+            tagAdapter.notifyDataSetChanged(); // Refresh the adapter
+            tagsToDelete.clear(); // Clear the selection
+            deleteMode = false; // Exit delete mode
+            deleteButton.setBackgroundColor(Color.WHITE); // Reset button color
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ADD_TAG_REQUEST_CODE && resultCode == RESULT_OK) {
+            String newTagName = data.getStringExtra("newTag");
+            tags.add(new Tag(newTagName));
+            tagAdapter.notifyDataSetChanged();
+        }
     }
 }
 
