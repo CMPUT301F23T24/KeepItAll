708a37053e64ab1de894cad934d97dab7bff6520
NicroFang <nicrofang@gmail.com>
2023-11-24 21:49:45 -0700

    camera is working bby, les go

--- app/src/main/java/com/example/keepitall/ImageGalleryActivity.java
+++ app/src/main/java/com/example/keepitall/ImageGalleryActivity.java
@@ -14,9 +14,13 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -30,9 +34,11 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Objects;
 
 /**
  * Class used to for the ImageGalleryActivity
@@ -42,13 +48,13 @@ import java.util.Date;
 public class ImageGalleryActivity extends AppCompatActivity {
     private ActivityResultLauncher<PickVisualMediaRequest> pickMultipleMedia;
     private String currentPhotoPath;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_IMAGE_CAPTURE = 3;
     private static final int PERMISSION_REQUEST_CODE = 2;
     private RecyclerView recyclerView;
     private TextView recyclerViewText;
     private ArrayList<Uri> uri = new ArrayList<>();
     private ImageRecyclerAdapter imageRecyclerAdapter;
-
+    private ImageView hiddenImage;
     private static final int Read_Permission = 1;
     private static final int PICK_IMAGE = 1;
 
@@ -60,6 +66,7 @@ public class ImageGalleryActivity extends AppCompatActivity {
         Button cameraButton = findViewById(R.id.cameraButton);
         Button galleryButton = findViewById(R.id.galleryOpenButton);
         recyclerViewText = findViewById(R.id.totalPhotos);
+        hiddenImage = findViewById(R.id.hiddenImage);
         recyclerView = findViewById(R.id.recyclerView_Gallery_Images);
         imageRecyclerAdapter = new ImageRecyclerAdapter(uri);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
@@ -127,13 +134,59 @@ public class ImageGalleryActivity extends AppCompatActivity {
                 recyclerViewText.setText("Total Photos: " + uri.size());
             } else if(data.getData() != null){
                 Toast.makeText(getApplicationContext(), "Single image selected", Toast.LENGTH_SHORT).show();
+                // get the URI of the image
                 String imageURL = data.getData().getPath();
                 uri.add(Uri.parse(imageURL));
             }
         }
+
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            // save the value of the images to hiddenImage, which we will use to save to the gallery
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            hiddenImage.setImageBitmap(imageBitmap);
+
+            SaveImageToGallery();
+            // set the bitmap to the image view
+
+        }
+
         imageRecyclerAdapter.notifyDataSetChanged();
         ///TODO: Change the itemLogo to the image that was selected (the first image in the list)
     }
+
+
+    private void SaveImageToGallery(){
+        // -- S
+        Toast.makeText(getApplicationContext(), "Image saved to gallery", Toast.LENGTH_SHORT).show();
+        Uri images;
+        ContentResolver contentResolver = getContentResolver();
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            images = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
+        } else{
+            images = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        }
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, System.currentTimeMillis() + ".jpg");
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/*");
+        Uri uri = contentResolver.insert(images, contentValues);
+
+        try {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) hiddenImage.getDrawable();
+            Bitmap bitmap = bitmapDrawable.getBitmap();
+
+            OutputStream outputStream = contentResolver.openOutputStream(Objects.requireNonNull(uri));
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            Objects.requireNonNull(outputStream);
+            Toast.makeText(getApplicationContext(), "Image saved to gallery", Toast.LENGTH_SHORT).show();
+
+        }catch(Exception e){
+            Toast.makeText(getApplicationContext(), "Image NOT saved to gallery!!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 
     private void OpenCamera(){
         // Check if the CAMERA permission is not granted
@@ -164,10 +217,14 @@ public class ImageGalleryActivity extends AppCompatActivity {
     }
 
     private void startCameraIntent(){
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
+
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
     }
+
+
+
+    //// ------------------------ ////
+
 }
 
